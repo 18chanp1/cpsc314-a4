e8de623d67204720c7fb31a700e41fffb9a98a94
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2024-03-24 14:39:16 -0700

    patched the bug

--- part1/glsl/envmap.fs.glsl
+++ part1/glsl/envmap.fs.glsl
@@ -11,6 +11,8 @@ void main( void ) {
 
   // Q1c : Calculate the vector that can be used to sample from the cubemap
   // TODO:
-  vec4 tcolor = textureCube(skybox, vcsNormal);
+  vec3 normal = normalize(vcsNormal);
+  vec3 reflected = reflect(normalize(vec3(-vcsPosition)), normal);
+  vec4 tcolor = textureCube(skybox, reflected);
   gl_FragColor = tcolor;
 }
